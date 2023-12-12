21afff87a0d2312dc7f0771270379fc950d97e73
Romain <romainbigalion@hotmail.fr>
2023-12-12 12:17:11 +0100

    ajout de mew ??? si pas d'image

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function PokemonCard () {
-    return (
-        <figure>
-            <img src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' alt = 'bulbizarre' />
-            <figcaption>Bulbasaur</figcaption>
-        </figure>
-    );
+    const pokemon = pokemonList[0]     
+    // création d'une constante pour récupérer les éléments de mon tableau 
+  return (
+
+<figure>
+    {pokemon.imgSrc ? (
+    <img src = {pokemon.imgSrc} alt={pokemon.name} />) : (
+        <p>???</p>
+    )}
+    {/*  condition ternaire  est-ce que pokemon.imgSrc est bien défini ? si oui tu m'affiches la src et l'alt(nom) sinon tu me mets ???*/}
+    <figcaption>{pokemon.name}</figcaption>
+    {/* dans tous les cas affiche moi le nom du pokémon à la fin de ma condition */}
+</figure>
+  );
+
 }
 export default PokemonCard;
+

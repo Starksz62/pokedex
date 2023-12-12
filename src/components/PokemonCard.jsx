8faa61cd1bd26c67bd01281e14737a6fce0c44b2
Romain <romainbigalion@hotmail.fr>
2023-12-12 11:48:20 +0100

    ajout de bulbizarre

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PokemonCard () {
+    return (
+        <figure>
+            <img src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' alt = 'bulbizarre' />
+            <figcaption>Bulbasaur</figcaption>
+        </figure>
+    );
+}
+export default PokemonCard;
